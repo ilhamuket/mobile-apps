20a2d81d694eb5c4892c6fd4c51d8a0f9dbf64be
mahardika22 <dikamahar884@gmail.com>
2021-07-31 16:35:19 +0700

    Vue 2

--- tari_fe/src/views/pages/dashboard/_forYou.vue
+++ tari_fe/src/views/pages/dashboard/_forYou.vue
@@ -21,7 +21,7 @@
             <iframe
               width="100%"
               height="400"
-              src="https://www.youtube.com/embed/liuSjQERYMk?autoplay=1&mute=1&rel=0&modestbranding=1&fs=1"
+              src="https://www.youtube.com/embed/liuSjQERYMk?autoplay=1&mute=1&rel=0&modestbranding=1&fs=1&loop=0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
               sandbox="allow-scripts allow-same-origin"
               allowfullscreen
