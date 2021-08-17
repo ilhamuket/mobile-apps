514d1bfa5703099b027abec8760959d2ea3fabeb
mahardika22 <dikamahar884@gmail.com>
2021-08-17 10:42:43 +0700

    fix

--- tari_fe/src/views/dashboard/components/core/AppBar.vue
+++ tari_fe/src/views/dashboard/components/core/AppBar.vue
@@ -11,7 +11,7 @@
     <v-btn
       v-if="!drawer"
       outlined
-      :class="drawer ? 'ml-6' : 'ml-6'"
+      class="'ml-6'"
       elevation="1"
       :color="$vuetify.theme.dark ? '' : '#283046'"
       fab
