5e75e9b34a49ef81d8f776e3bb37111f9e0a8f1d
mahardika22 <dikamahar884@gmail.com>
2021-09-06 19:04:21 +0700

    Go

--- tari_api/Modules/User/Notifications/VerifiedAccount.php
+++ tari_api/Modules/User/Notifications/VerifiedAccount.php
@@ -40,7 +40,7 @@ class VerifiedAccount extends Notification
      */
     public function toMail($notifiable)
     {
-        $url = env("FRONTEND_URL_PRODUCTION_MAIN_APP", "https://app.ensiklotari.com/") . 'verification';
+        $url = env("FRONTEND_URL_PRODUCTION_MAIN_APP", "https://main-app-ensiklotari.vercel.app/") . 'verification';
         return (new MailMessage)
             ->subject('Verify Your Account')
             ->line('Click Button Verify Email Address To Verification Your Account ..')
