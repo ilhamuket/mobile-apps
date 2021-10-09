f4124063577bb60abe67ce3ef2e195c137e970e9
mahardika22 <dikamahar884@gmail.com>
2021-10-09 13:33:25 +0700

    push

--- tari_studio/.eslintrc.js
+++ tari_studio/.eslintrc.js
@@ -7,16 +7,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'off',
-    // 'comma-dangle': [
-    //   'off',
-    //   {
-    //     arrays: 'never',
-    //     objects: 'never',
-    //     imports: 'never',
-    //     exports: 'never',
-    //     functions: 'never'
-    //   }
-    // ]
   },
   parserOptions: {
     parser: 'babel-eslint',
