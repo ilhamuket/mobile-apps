2d125f383fded7093b76b8d6032180c9a1749c41
mahardika22 <dikamahar884@gmail.com>
2021-10-09 13:36:18 +0700

    s

--- tari_studio/.eslintrc.js
+++ tari_studio/.eslintrc.js
@@ -7,6 +7,14 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'off',
+    sourceType: 'module',
+    allowImportExportEverywhere: false,
+    ecmaFeatures: {
+      globalReturn: false,
+    },
+    babelOptions: {
+      configFile: 'path/to/config.js',
+    },
   },
   parserOptions: {
     parser: 'babel-eslint',
