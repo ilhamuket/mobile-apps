a4bcc9e3a2c65c9f505edd9ae449aacbf4c4e1c5
mahardika22 <dikamahar884@gmail.com>
2021-10-11 00:47:02 +0700

    api

--- tari_api/Modules/Studio/Http/Controllers/ImagesStudioController.php
+++ tari_api/Modules/Studio/Http/Controllers/ImagesStudioController.php
@@ -119,8 +119,9 @@ class ImagesStudioController extends Controller
         try {
             $master = new ImagesStudio();
             $master->name_thumbnail = $request->name_thumbnail;
-            $path = $request->photo->store('images');
-            // $publicPath = \Storage::url($path);
+            $path = $request->photo->store('listImg');
+            // $publicPath = \Images::url($path);
+            dd($path);
             $master->url =  $path;
             $master->studio_id = $request->studio_id;
             $master->type = $request->type;
