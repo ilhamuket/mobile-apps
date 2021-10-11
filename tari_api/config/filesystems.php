8394c00a768fb74333916e73e5482a3bbd75390b
mahardika22 <dikamahar884@gmail.com>
2021-10-11 11:26:32 +0700

    files

--- tari_api/config/filesystems.php
+++ tari_api/config/filesystems.php
@@ -70,7 +70,7 @@ return [
     'links' => [
         public_path('storage') => storage_path('app/public'),
         public_path('images') => storage_path('app/images'),
-        // public_path('listImg') => storage_path('app/images/listImg'),
+        public_path('listImg') => storage_path('app/images/listImg'),
         // public_path('images/category') => storage_path('app/images/category')
     ],
 
