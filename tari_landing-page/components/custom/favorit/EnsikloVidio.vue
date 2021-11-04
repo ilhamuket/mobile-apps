6ac9e12cb4af9e9c8a3d675070001b54dfa6c32e
MahardiikaKD <dikamahar884@gmail.com>
2021-11-04 16:00:53 +0700

    koreksi

--- tari_landing-page/components/custom/favorit/EnsikloVidio.vue
+++ tari_landing-page/components/custom/favorit/EnsikloVidio.vue
@@ -9,7 +9,7 @@
           <v-col cols="12" sm="10" md="9" lg="7">
             <div class="text-center">
               <h2 class="section-title font-weight-medium">
-                EnsikloVidio
+                EnsikloVideo
               </h2>
               <!-- <p>
                 You can relay on our amazing features list and also our customer
