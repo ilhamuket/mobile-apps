b06a77a69e9688e0e4e9b720d99651e8b43d8c84
MahardiikaKD <dikamahar884@gmail.com>
2021-11-12 13:33:31 +0700

    push

--- tari_api/Modules/Studio/Http/Controllers/StudioClassVidiosController.php
+++ tari_api/Modules/Studio/Http/Controllers/StudioClassVidiosController.php
@@ -18,7 +18,7 @@ class StudioClassVidiosController extends Controller
         try {
             $master = StudioClassVidios::entities($request->entities)
                 ->where('is_verified', true)
-                ->whereNull('delete_at')
+                ->where('deleted_at', '!=', null)
                 ->paginate($request->input("paginate", 6));
 
             return Json::response(($master));
