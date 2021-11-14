a234e1569b1cabd150fab94494e50ec8df2a2069
MahardiikaKD <dikamahar884@gmail.com>
2021-11-14 15:32:42 +0700

    push for landing-page

--- tari_landing-page/components/shared/lp-banner/Banner.vue
+++ tari_landing-page/components/shared/lp-banner/Banner.vue
@@ -8,7 +8,7 @@
         <v-col cols="12" md="7" lg="6" class="d-flex align-center">
           <div class="text-center text-md-left">
             <h1 class="banner-title font-spartan font-weight-bold primary--text1">
-              Selamat Datang EnsikloTari
+              Selamat Datang <br> Di EnsikloTari
             </h1>
             <!-- <h4 class="banner-subtitle white--text font-weight-regular">
               Free Vuetify UI Kit with Lots of Ready to Use Sections
@@ -40,9 +40,9 @@
             </div>
           </div>
         </v-col>
-        <v-col cols="12" md="5" lg="6">
+        <v-col cols="12" md="5" lg="6" class="">
           <v-img
-            src="https://api.ensiklotari.com/images/category/0EJblVJPOqzE2WcOSP3CANM8hsTX1YJ3hvZOfJ2N.svg"
+            :src="require('@/assets/images/dance.svg')"
             alt="banner"
           />
         </v-col>
--- tari_landing-page/nuxt.config.js
+++ tari_landing-page/nuxt.config.js
@@ -15,7 +15,7 @@ export default {
       { hid: "description", name: "description", content: "" },
       { name: "format-detection", content: "telephone=no" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/logo.png" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/ensiklo.png" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
