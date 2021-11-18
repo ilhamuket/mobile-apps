b875593fb736fe255aa46e69bc0c5dc6ada237bd
MahardiikaKD <dikamahar884@gmail.com>
2021-11-18 12:15:37 +0700

    push

--- tari_api/Modules/Studio/Entities/StudioClassVidios.php
+++ tari_api/Modules/Studio/Entities/StudioClassVidios.php
@@ -5,10 +5,11 @@ namespace Modules\Studio\Entities;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class StudioClassVidios extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [];
     protected $table = 'studio_class_vidios';
