b27c7b5c73383d8a58fab4b55d37bbc80abd1434
MahardiikaKD <dikamahar884@gmail.com>
2021-11-25 17:04:36 +0700

    img

--- tari_api/Modules/StudioOwners/Entities/ImgInstructor.php
+++ tari_api/Modules/StudioOwners/Entities/ImgInstructor.php
@@ -19,8 +19,8 @@ class ImgInstructor extends Model
     public function getUrlAttribute()
     {
         if ($this->attributes['url'] != null) {
-            return  env('IMAGE_URL', ' http://127.0.0.1:8000/') . $this->attributes['url'];
-            // return  env('IMAGE_URL', ' https://api.ensiklotari.com/') . $this->attributes['url'];
+            // return  env('IMAGE_URL', ' http://127.0.0.1:8000/') . $this->attributes['url'];
+            return  env('IMAGE_URL', ' https://api.ensiklotari.com/') . $this->attributes['url'];
         } else {
             return $this->attributes['url'];
         }
