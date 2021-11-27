2994f01976984eb79fa6cb3080614e4f2274a69b
MahardiikaKD <dikamahar884@gmail.com>
2021-11-28 00:41:43 +0700

    push

--- tari_landing-page/dist/vuetify.options.js
+++ tari_landing-page/dist/vuetify.options.js
@@ -1,25 +1,39 @@
+const theme = {
+  primary: '#9DC4D1',
+  secondary: '#8F8F38',
+  third: '294F50',
+  accent: '#9C27b0',
+  info: '#00CAE3',
+  pallet1: '#843839',
+  landingpage: '#171B34',
+  btn_primary: '#4CAF50',
+}
 export default {
   theme: {
-    dark: false,
     themes: {
-      dark: {
-        primary: "#9DC5D1",
-        accent: "#607df9",
-        secondary: "#a73aeb",
-        info: "#0164d3",
-        warning: "#fec500",
-        error: "#ff4d7e",
-        success: "#2cdd9b"
-      },
-      light: {
-        primary: "#9DC5D1",
-        accent: "#607df9",
-        secondary: "#a73aeb",
-        info: "#0164d3",
-        warning: "#fec500",
-        error: "#ff4d7e",
-        success: "#2cdd9b"
-      }
-    }
+      dark: theme,
+      light: theme,
+    },
+    dark: false,
+    // themes: {
+    //   dark: {
+    //     primary: "#9DC5D1",
+    //     accent: "#607df9",
+    //     secondary: "#a73aeb",
+    //     info: "#0164d3",
+    //     warning: "#fec500",
+    //     error: "#ff4d7e",
+    //     success: "#2cdd9b"
+    //   },
+    //   light: {
+    //     primary: "#9DC5D1",
+    //     accent: "#607df9",
+    //     secondary: "#a73aeb",
+    //     info: "#0164d3",
+    //     warning: "#fec500",
+    //     error: "#ff4d7e",
+    //     success: "#2cdd9b"
+    //   }
+    // }
   }
 };
