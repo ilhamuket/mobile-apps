75c35145cd0ab600a3bd942c5d4aebb1ac4d8bd6
MahardiikaKD <dikamahar884@gmail.com>
2021-12-31 16:26:14 +0700

    push

--- tari_landing-page/components/shared/lp-banner/Banner.vue
+++ tari_landing-page/components/shared/lp-banner/Banner.vue
@@ -13,10 +13,10 @@
             <!-- <h4 class="banner-subtitle white--text font-weight-regular">
               Free Vuetify UI Kit with Lots of Ready to Use Sections
             </h4> -->
-            <div class="mt-16 pt-2">
+            <div class="mt-12 pt-2">
               <v-btn
                 color="#4CAF50"
-                class="mr-0 mr-md-8 mb-5 btn-masuk mb-md-0 btn-custom-md white--text font-weight-bold"
+                class="mr-0 mr-md-8 mb-5 btn-masuk mobile__btn mb-md-0 btn-custom-md white--text font-weight-bold"
                 nuxt
                 large
                 target="_blank"
@@ -30,7 +30,7 @@
                 large
                 target="__blank"
                 href="https://studio.ensiklotari.com"
-                class="btn-custom-md font-weight-bold"
+                class="btn-custom-md font-weight-bold mobile__btn"
                 outlined
                 color="#9DC4D1"
                 elevation="0"
@@ -71,4 +71,8 @@ export default {
         .banner-title
           padding-top: 3%
           padding-left: 14% 
+      &__btn
+        margin-left: 5% !important
+        width: 89%
+        margin-bottom: 6% !important
 </style>
